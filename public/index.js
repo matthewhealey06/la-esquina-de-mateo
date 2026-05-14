eb9982406d1634c4efdc1140d36aadf878feba95
Matthew Healey <matthewhealey06@gmail.com>
2026-05-14 03:49:29 +0100

    initial commit: hero section & intial menu display

---
+++ public/index.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('.nav-right');
+const menuOverlay = document.querySelector('.menu-overlay');
+
+menuButton.addEventListener('click', () => {
+  menuOverlay.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+});
