38bcede2d496c4ed81e7dff4e4e2604c26691aa0
Matthew Healey <matthewhealey06@gmail.com>
2026-05-15 15:28:01 +0100

    add menu items | Improve mobile responsiveness

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
-const menuButton = document.querySelector('.nav-right');
-const menuOverlay = document.querySelector('.menu-overlay');
+const menuButton = document.querySelector(".nav-right");
+const menuOverlay = document.querySelector(".menu-overlay");
 
-menuButton.addEventListener('click', () => {
-  menuOverlay.classList.toggle('active');
-  document.body.classList.toggle('no-scroll');
+menuButton.addEventListener("click", () => {
+  menuOverlay.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
 });
