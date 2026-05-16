97dfe3c50a179955e1c371ce73933881b41aef05
Matthew Healey <matthewhealey06@gmail.com>
2026-05-16 21:13:02 +0100

    add 'our story' section with JS implementation

--- public/index.js
+++ public/index.js
@@ -4,4 +4,29 @@ const menuOverlay = document.querySelector(".menu-overlay");
 menuButton.addEventListener("click", () => {
   menuOverlay.classList.toggle("active");
   document.body.classList.toggle("no-scroll");
+  document.documentElement.classList.toggle("no-scroll");
+});
+
+const arrows = document.querySelector('.timeline-arrows');
+const dots = document.querySelectorAll('.dot');
+const years = document.querySelectorAll('.timeline-years p');
+let currentIndex = 0;
+const totalSlides = 3;
+const track = document.querySelector('.story-wrapper');
+
+arrows.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const direction = e.target.dataset.direction;
+    if (direction === 'right' && currentIndex < totalSlides - 1) {
+      currentIndex++;
+    } else if (direction === 'left' && currentIndex > 0) {
+      currentIndex--;
+    }
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[currentIndex].classList.add('active');
+    years.forEach(year => year.classList.remove('active'));
+    years[currentIndex].classList.add('active');
+    const cardWidth = document.querySelector('.story-card').offsetWidth + 50;
+    track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+  }
 });
